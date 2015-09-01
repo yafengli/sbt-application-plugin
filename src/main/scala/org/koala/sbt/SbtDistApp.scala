ea7ff7eefc9425022ec686634430d5871283b3fb
ya_feng_li <ya_feng_li@163.com>
2015-09-01 11:20:26 +0800

    1.1.1 is ok.

--- src/main/scala/org/koala/sbt/SbtDistApp.scala
+++ src/main/scala/org/koala/sbt/SbtDistApp.scala
@@ -39,34 +39,35 @@ object SbtDistApp extends AutoPlugin {
     distZip <<= (packageBin in Compile, crossTarget, dependencyClasspath in Compile, dirSetting, mainClass, organization, name, version) map {
       (p, out, dr, ds, mc, org, name, v) =>
         try {
-          implicit val buffers = mutable.Buffer[(File, String)]()
+          implicit val map = mutable.HashMap[String, File]()
+
           //dependencies jar package jar module dependOn jar
           dr.map(_.data).foreach {
-            case f: File if f.isFile && f.name.endsWith(".jar") => buffers += f -> s"lib/${f.name}"
+            case f: File if f.isFile && f.name.endsWith(".jar") => map += s"lib/${f.name}" -> f
             case d: File if d.isDirectory =>
               d.getParentFile.listFiles().filter(filter).headOption match {
-                case Some(f) => buffers += f -> s"lib/${f.name}"
-                case None => println(s":ERR:${d.getParentFile.absolutePath} NOT FOUND JAR FILE.ADD [exportJar := true] TO SETTING.")
+                case Some(f) => map += s"lib/${f.name}" -> f
+                case None => println(s":ERR:${d.getParentFile.absolutePath} NOT FOUND JAR FILE.ADD [exportJars := true] TO SETTING.")
               }
           }
           if (out.listFiles() != null && out.listFiles().filter(filter).headOption.isDefined) {
             val f = out.listFiles().filter(filter).head
-            buffers += f -> s"lib/${f.name}"
+            map += s"lib/${f.name}" -> f
           } else println(s":ERR:${out.absolutePath} NOT FOUND JAR FILE.ADD [exportJar := true] TO SETTING.")
           //copy dirSetting files.
           ds.map(new File(_)).foreach {
             f =>
-              if (f.isDirectory) f.listFiles().foreach(copy(_, f.name)) else buffers += f -> f.name
+              if (f.isDirectory) f.listFiles().foreach(copy(_, f.name)) else if (!map.contains(f.name)) map += f.name -> f
           }
           //run shell
-          if (mc.isDefined) buffers ++= <::((out / s"${name}.bat", SbtDistAppShell.windows, Map('mainClass -> mc.get)), (out / s"${name}", SbtDistAppShell.linux, Map('mainClass -> mc.get)))
+          if (mc.isDefined) map ++= <<:((out / s"${name}.bat", SbtDistAppShell.windows, Map('mainClass -> mc.get)), (out / s"${name}", SbtDistAppShell.linux, Map('mainClass -> mc.get)))
 
           val dist = (out / s"../universal/${org}-${name}-${v}.zip")
 
           //loop buffers
-          //buffers.foreach(t => println(t._1.absolutePath))
+          //map.foreach(t => println(t._2.absolutePath))
 
-          IO.zip(buffers, dist)
+          IO.zip(map.map(e => e._2 -> e._1), dist)
           IO.unzip(dist, (out / "../universal/stage"))
         } catch {
           case e: Exception => e.printStackTrace()
@@ -90,10 +91,10 @@ object SbtDistApp extends AutoPlugin {
         d.foreach(f => println(f.data.getAbsolutePath))
     })
 
-  def copy(file: File, prefix: String)(implicit buffers: mutable.Buffer[(File, String)]): Unit = {
+  def copy(file: File, prefix: String)(implicit map: mutable.HashMap[String, File]): Unit = {
     try {
       file match {
-        case f: File if f.isFile => buffers += f -> path(prefix, f)
+        case f: File if f.isFile => map += path(prefix, f) -> f
         case d: File if d.isDirectory => d.listFiles().foreach(f => copy(f, path(prefix, d)))
         case _ =>
       }
@@ -109,12 +110,12 @@ object SbtDistApp extends AutoPlugin {
     try pw.write(replaceTemplates(s, templates)) finally pw.close()
   }
 
-  private def <::(ps: (File, String, Map[Symbol, String])*): Seq[(File, String)] = {
+  private def <<:(ps: (File, String, Map[Symbol, String])*): Map[String, File] = {
     ps.map {
       t =>
         writeToFile(t._1, t._2, t._3)
-        (t._1, s"bin/${t._1.name}")
-    }
+        s"bin/${t._1.name}" -> t._1
+    } toMap
   }
 
   private def path(prefix: String, f: File): String = {
